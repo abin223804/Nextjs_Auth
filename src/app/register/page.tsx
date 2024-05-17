26cf5f828e217806cda988387607158c3fd67fef
Abin s chandran <abinschandran@gmail.com>
2024-05-17 15:58:33 +0530

    api created for register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,25 @@
+
+"use client"
+
+
 import React from 'react'
 import Link from 'next/link'
 
 const page = () => {
+
+    const handleSubmit= async (e:any) => {
+        e.preventDefault(); 
+
+        const email = e.target[0].value;
+        const password = e.target[1].value;
+        console.log(email,password);
+        
+
+    }
+
+
+
+
   return (
    
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -11,7 +29,7 @@ const page = () => {
         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
           Create an account
         </h1>
-        <form className="space-y-4 md:space-y-6" action="#">
+        <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6" action="#">
           <div>
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
             <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
