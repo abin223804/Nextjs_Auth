b62a2c06561e8e04af659e29afe440fa206b6727
Abin s chandran <abinschandran@gmail.com>
2024-05-17 14:53:46 +0530

    routes page created and registerforms added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <div>
+        <ul className="flex justify-between  m-10 item-center">
+        <div>
+            <Link href="/">
+                <li>Home</li>
+            </Link>
+        </div>
+        <div  className="flex gap-10">
+            <Link href="/dashboard">
+                <li>dashboard</li>
+            </Link>
+        </div>
+        <div>
+            <Link href="/login">
+                <li>Login</li>
+            </Link>
+        </div>
+        <div>
+            <Link href="/register">
+                <li>Register</li>
+            </Link>
+        </div>
+
+        </ul>
+        </div>
+  )
+}
+
+export default Navbar
