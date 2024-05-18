0a0f292f4c99b752a7e92c55809cd15cc3bda0df
Abin s chandran <abinschandran@gmail.com>
2024-05-18 11:59:38 +0530

    auth function added

---
+++ src/app/api/register/auth/[...nextauth]/route.ts
@@ -0,0 +1,55 @@
+import NextAuth from "next-auth";
+import { Account, User as AuthUser } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import bcrypt from "bcryptjs";
+import User from "@/models/user";
+import connect from "@/utils/db";
+
+export const authOptions: any = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+        },
+        password: {
+          label: "password",
+          type: "password",
+        },
+      },
+
+      async authorize(credentials: any) {
+        await connect();
+
+        try {
+          const user = await User.findOne({ email: credentials.email });
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+            if (isPasswordCorrect) {
+              return user;
+            }
+          }
+          // return null;
+        } catch (error) {
+          throw new Error(error);
+        }
+      },
+    }),
+
+    GithubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
